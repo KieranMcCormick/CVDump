636edf4c97bb46cc2350af1d466c3ddfe70f2e08
ykc10 <ykc10@sfu.ca>
2017-10-28 21:25:08 -0700

    testing comment creation

--- src/app/js/containers/App/comments.jsx
+++ src/app/js/containers/App/comments.jsx
@@ -1,12 +1,20 @@
-import React, { Component, } from "react"
+import React, { Component } from "react"
 
 class Comment extends Component {
+ // UI representation of a Comment box, takes in a comment object and displays the date,data, and author and length of reply thread
+ constructor(props) {
+    super(props);
+    this.state = {currentDate: new Date()};
+  }
     render() {
         return (
             <div className="comment">
-                <header className="app-header">
-                    <h1>This is a comment </h1>
-                </header>
+               <h1> Comment </h1>
+               <p class="comment_date"> {this.props.comment} </p>
+               <p class="comment_author"> By: {this.props.comment.author} </p>
+               <p class="comment_data">{this.props.comment.data} </p>
+               <p> Replies ({this.props.comment.thread.length})</p>
+
             </div>
         )
     }
