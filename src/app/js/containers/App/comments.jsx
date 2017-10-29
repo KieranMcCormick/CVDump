df003b77c33f1d0f7e2ac4d2a521c6a0caf4fd88
ykc10 <ykc10@sfu.ca>
2017-10-28 21:17:08 -0700

    experimenting with comments

---
+++ src/app/js/containers/App/comments.jsx
@@ -0,0 +1,14 @@
+import React, { Component, } from "react"
+
+class Comment extends Component {
+    render() {
+        return (
+            <div className="comment">
+                <header className="app-header">
+                    <h1>This is a comment </h1>
+                </header>
+            </div>
+        )
+    }
+}
+export default Comment
