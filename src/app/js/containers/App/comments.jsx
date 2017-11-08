b06e230b89fdb74dcdd813022f3f68ba4f2c3eaa
unknown <ykc10@sfu.ca>
2017-11-08 15:44:46 -0800

    merge conflicts

--- src/app/js/containers/App/comments.jsx
+++ src/app/js/containers/App/comments.jsx
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-import React, { Component } from 'react'
-
-class Comment extends Component {
-    // UI representation of a Comment , takes in a comment object and displays the date,data, and author and length of reply thread
-    constructor(props) {
-        super(props)
-        this.state = {currentDate: new Date()}
-    }
-    render() {
-        return (
-            <div className="comment">
-                <h1> Comment </h1>
-                <p className="comment_date"> {this.props.comment.date} </p>
-                <p className="comment_author"> By: {this.props.comment.author} </p>
-                <p className="comment_data">{this.props.comment.data} </p>
-            </div>
-        )
-=======
 import React, { Component } from "react"
 
 class Comment extends Component {
@@ -34,7 +15,6 @@ class Comment extends Component {
                <p class="comment_data">{this.props.comment.data} </p>
             </div>
         );
->>>>>>> c8ae984610b96cf0703563b60e062d49db419b6d
     }
 }
 export default Comment
