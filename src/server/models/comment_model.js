e7090ac55341155e88b0d16595ceaf707fe85534
ykc10 <ykc10@sfu.ca>
2017-11-11 18:38:17 -0800

    added api model for comments

--- src/server/models/comment_model.js
+++ src/server/models/comment_model.js
@@ -1,20 +1,20 @@
-var express = require('express')
-const db = require('../db.js')
-var router = express.Router();
-const bodyParser  = require('body-parser')
+const express = require('express')
+//const db = require('../db.js')
+const router = express.Router()
+//const bodyParser  = require('body-parser')
 
-
-router.get('/get/:documentId',(req,res) =>{
-    console.log("fetch comments");
+router.get('/get/:documentId',(req,res) => {
+    console.log('fetch comments')
     console.log(req.params.documentId);
-    res.send({message:"okay"});
-    return;
+    res.send({message:'okay'})
+    return
 })
 
 router.post('/create',(req,res) =>{
-    console.log("create comment")
-    console.log(req.body);
-    res.send({comments:"okay"});
+    console.log('create comment')
+    console.log(req.body)
+    res.send({comments:'okay'});
     return
 })
+
 module.exports = router
