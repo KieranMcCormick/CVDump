073aeb74ea5069bac28858cb6a6371099d06fa10
seleenah <seleenah@sfu.ca>
2017-11-17 21:23:59 -0800

    files

--- src/server/index.js
+++ src/server/index.js
@@ -61,6 +61,7 @@ require('./handlers/passport')(app)
 app.use(require('./routes/auth'))
 app.use(require('./routes/sessions'))
 app.use('/comment', require('./routes/comment_api'))
+app.use('/file', require('./routes/files_api'))
 require('./routes')(app)
 
 app.get('*', (request, response) => {
---
+++ src/server/routes/files_api.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const router  = express.Router()
+const fs      = require('../fs/fs.js')  
+
+router.get('/savepdf/:doc_id?', (req, res, next) => {
+
+    const doc_id    = req.params.doc_id
+    //const curr_user = idk
+
+    if(doc_id){
+        //autheticate user before returning file   
+        next()
+    }
+    else{
+        next(new Error("No such document"))
+    }
+})
+
+router.get('/savepdf', function(req, res){
+
+})
+
+module.exports = router
