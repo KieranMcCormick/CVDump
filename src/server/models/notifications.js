424843676aeb231b84a94c15d1972e1cd63c11b7
ykc10 <ykc10@sfu.ca>
2017-11-21 21:22:30 -0800

    working on loading notifications

--- src/server/models/notifications.js
+++ src/server/models/notifications.js
@@ -1,6 +1,6 @@
 const { sqlInsert, sqlSelect } = require('../db')
 const _ = require('lodash')
-
+const fetchUserQuery = 'SELECT uuid FROM users WHERE username = ?'
 class Notifications {
     constructor(props) {
         if (props) {
@@ -8,26 +8,27 @@ class Notifications {
             this.documentId = props.documentId ? props.documentId : null
             this.timeStamp = props.timeStamp ? props.timeStamp : ''
             this.type = props.type ? props.type : "system"
+            
     }
     //checks if document exists
    
     //checks if user exists ,returns iD
    }
 
-  
+   
 
     create() {
         console.log(this);
         // might query using username instead ?
         const createQuery = 'INSERT INTO notifications (uuid, user_id, document_id, type, created_at) VALUES (UUID(), ?, ?, ?, ?)'
-        const fetchQuery = 'SELECT uuid FROM users WHERE username = ?'
+       
         return new Promise((resolve, reject) => {
             // notifications has to have userid, docid and a type of notification
             if( !this.targetUser || !this.documentId || !this.type) {
                 return reject({message:"Missing userID and docID"})
             }
 
-            sqlSelect(fetchQuery, [this.targetUser], (err, success) => {
+            sqlSelect(fetchUserQuery, [this.targetUser], (err, success) => {
                     if (err) {
                         console.log(err)
                         return reject({ message: 'No such user' })
@@ -50,15 +51,26 @@ class Notifications {
 
     load() {
         let that = this
-        const fetchQuery = 'SELECT user_id, type, created_at, document_id FROM notifications WHERE user_id = ?'
+        const fetchNotificationQuery = 'SELECT user_id, type, created_at, document_id FROM notifications WHERE user_id = ?'
+        
         return new Promise((resolve, reject) => {
-            sqlSelect(fetchQuery, [this.targetUser], (err, success) => {
+            sqlSelect(fetchUserQuery, [this.targetUser], (err, success) => {
                 if (err) {
                     console.log(err)
-                    return resolve({ message: 'No comments' })
+                    return reject({ message: 'No such user' })
                 }
+
                 if (success) {
-                    return resolve(that.parseCommentOutput(success))
+                    let uuid = success[0].uuid
+                    sqlSelect(fetchNotificationQuery, [uuid], (err, result) => {
+                        if(err) {
+                            return reject({message:"fail to fetch notifications"})
+                        }
+                        if(result){
+                            console.log(result)
+                            return resolve({notifications:result})
+                        }
+                    })
                 }
             })
         })
