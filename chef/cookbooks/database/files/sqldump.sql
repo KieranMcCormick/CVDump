ea7b5d77c4fbd035ac9a98525ea606b08f2b4951
unknown <ykc10@sfu.ca>
2017-11-21 16:43:30 -0800

    added notifications table to sqldump

--- chef/cookbooks/database/files/sqldump.sql
+++ chef/cookbooks/database/files/sqldump.sql
@@ -169,6 +169,35 @@ UNLOCK TABLES;
 --
 -- Table structure for table `users`
 --
+DROP TABLE IF EXISTS `notifications`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `notifications` (
+  `uuid` varchar(36) NOT NULL,
+  `type` varchar(36),
+  `user_id` varchar(36) NOT NULL,
+  `document_id` varchar(36),
+  `created_at` datetime NOT NULL,
+  `is_deleted` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`uuid`),
+  UNIQUE KEY `uuid` (`uuid`),
+  CONSTRAINT `notifications_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`uuid`),
+  CONSTRAINT `notifications_ibfk_2` FOREIGN KEY (`document_id`) REFERENCES `documents` (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `notifications` WRITE;
+/*!40000 ALTER TABLE `notifications` DISABLE KEYS */;
+/*!40000 ALTER TABLE `notifications` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+
+
 
 DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -208,3 +237,7 @@ UNLOCK TABLES;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
 -- Dump completed on 2017-11-25 18:06:09
+-- Dump completed on 2017-11-19  0:08:33
+
+
+
