a94ee3e7c16cd8011b3df3b353e3e5c82dfd73d8
Seleena Hua <seleenah@sfu.ca>
2017-11-22 22:06:04 -0800

    chef fs stuff

---
+++ chef/cookbooks/baseconfig/recipes/fs.rb
@@ -0,0 +1,35 @@
+# small scale file system setup
+
+directory 'home/ubuntu/files' do
+  owner node['runas_user']
+  recursive true
+  mode '764'
+  action :create
+end
+
+[1,2,3].each do |n|
+  directory "home/ubuntu/files/#{n}" do
+    owner node['runas_user']
+    recursive true
+    mode '764'
+    action :create
+  end
+  [1,2,3].each do |j|
+    directory "home/ubuntu/files/#{n}/#{j}" do
+      owner node['runas_user']
+      recursive true
+      mode '764'
+      action :create
+    end
+    [1,2,3].each do |k|
+      directory "home/ubuntu/files/#{n}/#{j}/#{k}" do
+        owner node['runas_user']
+        recursive true
+        mode '764'
+        action :create
+      end
+    end
+  end
+end
+
+
