45e361a2fa5091a75fbee3410292697566092c54
ykc10 <ykc10@sfu.ca>
2017-11-30 19:36:26 -0800

    merge errors thast didnt show up as a conflict

--- src/server/routes/api.js
+++ src/server/routes/api.js
@@ -5,12 +5,9 @@ router.use(require('./sessions'))
 router.use('/comment', require('./comment_api'))
 router.use('/files', require('./files_api'))
 router.use('/users', require('./users'))
-<<<<<<< HEAD
 router.use('/notifications',require('./notification_api'))
 router.use('/shared', require('./shared_api'))
-=======
 router.use('/shared', require('./shared_api'))
 router.use('/blocks', require('./blocks_api'))
->>>>>>> origin/master
 
 module.exports = router
